793b3b195aa5975b48de183e67a8aee15d52c94c
nathan <jnmichie@protonmail.com>
2021-01-07 07:40:56 -0600

    Fixed float issue with calculator.rb

--- RB101/lesson2/calculator.rb
+++ RB101/lesson2/calculator.rb
@@ -5,17 +5,17 @@ def calculator
   puts "Welcome to Calculator!"
   puts
   print "Input first number: "
-  num1 = gets.chomp.to_i
+  num1 = gets.chomp
   print "Input second number: "
-  num2 = gets.chomp.to_i
+  num2 = gets.chomp
   print "Input operation (add, subtract, etc.): "
   operation_string = gets.chomp
 
   case operation_string
-  when "add" then puts(num1 + num2)
-  when "subtract" then puts(num1 - num2)
-  when "multiply" then puts(num1 * num2)
-  when "divide" then puts(num1 / num2)
+  when "add" then puts(num1.to_i + num2.to_i)
+  when "subtract" then puts(num1.to_i - num2.to_i)
+  when "multiply" then puts(num1.to_i * num2.to_i)
+  when "divide" then puts(num1.to_f / num2.to_f)
   else puts "Invalid opration!"
   end
 
