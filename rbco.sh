204855223e84cd24b2f88289aa98937272b77c7d
loreandstory <loreandstory@outlook.com>
2021-02-03 20:23:39 -0600

    Add ruby code outputer (rbco.sh) for excersize submital.

---
+++ rbco.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Push ruby file and markup to log.txt
+printf "\`\`\`ruby\n" >> log.txt
+cat $1 >> log.txt
+printf "\`\`\`\n" >> log.txt
+
+
+# Push ruby file output and markup to log.txt
+printf "\nOutput:\n\`\`\`ruby\n" >> log.txt
+script -q -c "ruby $1" output.txt  # script adds header and footer to generated file. Removed in lines below.
+head -n -2 output.txt >> output_trimb.txt
+tail -n +2 output_trimb.txt >> output_trimmed.txt
+cat output_trimmed.txt >> log.txt
+printf "\`\`\`" >> log.txt
+
+# Copy log.txt to clipboard and remove log.txt file
+xclip -sel clip < log.txt
+rm log.txt output.txt output_trimb.txt output_trimmed.txt
+
